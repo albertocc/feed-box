b60a7d07b752c6b95fef8897fad6714dd8c4e8d3
albertocc <hola@alberto.cc>
2020-03-10 21:01:37 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,45 @@
+const { GistBox } = require('gist-box')
+const Parser = require('rss-parser')
+const parser = new Parser()
+const maxLength = 40 // 55 per line
+const { GITHUB_TOKEN: token, GIST_ID: id, RSS_URL: url } = steps.env;
+
+(async () => {
+  const feed = await parser.parseURL(url)
+
+  let content = ''
+  feed.items.slice(0, 5).forEach(item => {
+    content += `${trimTitle(item.title)} 📆 ${parseDate(item.pubDate)}\n`
+  })
+
+  const box = new GistBox({ id, token })
+  await box.update({
+    filename: feed.title,
+    description: ':pencil: A new description',
+    content: content
+  })
+  // console.log(feed.title)
+  // feed.items.slice(0, 5).forEach(item => {
+  //   console.log(`* ${trimTitle(item.title)} (${parseDate(item.pubDate)})`)
+  // })
+})()
+
+const parseDate = date => {
+  return new Date(date).toLocaleString('default', {
+    day: '2-digit',
+    month: 'short',
+    year: 'numeric'
+  })
+}
+
+const trimTitle = string => {
+  return string.length > maxLength
+    ? (string.substring(0, maxLength - 3).trimEnd() + '...').padEnd(maxLength)
+    : addSpaces(string)
+}
+
+const addSpaces = string => {
+  return string.length < maxLength
+    ? string + ' '.repeat(maxLength - string.length)
+    : string
+}
